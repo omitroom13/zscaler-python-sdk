1c8b4bc24b522bcd61b60fea21712be260e12b29
omitroom13 <22601789+omitroom13@users.noreply.github.com>
2020-10-12 02:53:36 +0900

    use fire. support all URL Categories API

--- zia/url_categories.py
+++ zia/url_categories.py
@@ -3,59 +3,58 @@ import logging
 import json
 import sys
 
+import fire
+
 from .defaults import *
 from . import load_config
 from .session import Session, RequestError
 
 class UrlCategories(object):
-    def __init__(self, session):
-        self.session = session
+    def __init__(self, session, output_type='dict'):
+        self._session = session
+        #json or string
+        self._output_type = output_type
+    def _output(self, res):
+        if self._output_type == 'dict':
+            return res
+        return json.dumps(res, indent=True)
     def list(self, custom_only=False):
-        # [{id, urls[]}]
         path = 'urlCategories'
         if custom_only:
             path += '?customOnly=true'
-        return self.session.get(path)
+        return self._output(self._session.get(path))
     def create(self, category):
         path = 'urlCategories'
         body = category
-        return self.session.post(path, body)
+        return self._output(self._session.post(path, body))
     def list_lite(self):
-        pass
+        path = 'urlCategories/lite'
+        return self._output(self._session.get(path))
     def get_quota(self):
-        pass
-    def update(self):
-        pass
-    def delete(self):
-        pass
-    def lookup(self):
-        pass
+        path = 'urlCategories/urlQuota'
+        return self._output(self._session.get(path))
+    def get(self, category_id):
+        path = 'urlCategories/{}'.format(category_id)
+        return self._output(self._session.get(path))
+    def update(self, category_id, category):
+        path = 'urlCategories/{}'.format(category_id)
+        return self._output(self._session.put(path, category))
+    def delete(self, category_id):
+        path = 'urlCategories/{}'.format(category_id)
+        return self._output(self._session.delete(path))
+    def lookup(self, urls):
+        path = 'urlLookup'
+        return self._output(self._session.post(path, urls))
 
 LOGGER = logging.getLogger(__name__)
 if __name__ == '__main__':
-    load_config()
-    LOGGER.setLevel(logging.DEBUG)
-    session = Session()
-    categories = UrlCategories(session)
-    session.authenticate()
-    l = categories.list()
-    LOGGER.info(len(l))
-    l = categories.list(True)
-    LOGGER.info(len(l))
-    category = {
-        "configuredName": "online blogs test",
-        "customCategory": "true",
-        "superCategory": "NEWS_AND_MEDIA",
-        "type": "URL_CATEGORY",
-        "description": "Description",
-        "editable": "false",
-        "keywords": ["test"],
-        "urls": [
-            "livejournal.com"
-        ]
-    }
     try:
-        LOGGER.info(categories.create(category))
+        load_config()
+        LOGGER.setLevel(logging.DEBUG)
+        session = Session()
+        categories = UrlCategories(session, 'str')
+        session.authenticate()
+        fire.Fire(categories)
     except RequestError as exc:
         fmt = 'method {} path {} code {} message {} body {}'
         LOGGER.error(fmt.format(exc.method, exc.path, exc.code, exc.message, exc.body))
